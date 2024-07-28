d8df107524bd7b8976a93efc2db3e680ec01f75b
Nada-ibrahim12 <123187920+Nada-ibrahim12@users.noreply.github.com>
2024-07-28 23:36:07 +0300

    delete book & local storage

--- Form Data/js/script.js
+++ Form Data/js/script.js
@@ -4,8 +4,15 @@ var BookCat = document.getElementById("BookCat")
 var BookDesc = document.getElementById("BookDesc")
 var tableBody = document.getElementById("tableBody")
 // var BookNa = document.getElementById("BookNa")
+
 var bookCont = []
-function getBook() {
+if (localStorage.getItem("allBooks") != null) {
+    bookCont = JSON.parse(localStorage.getItem("allBooks"))
+    displayBooks(bookCont)
+}
+
+// add book function
+function addBook() {
     var book = {
         name: BookNa.value,
         price: BookPr.value,
@@ -13,7 +20,8 @@ function getBook() {
         description: BookDesc.value
     }
     bookCont.push(book)
-    displayBooks()
+    localStorage.setItem("allBooks", JSON.stringify(bookCont))
+    displayBooks(bookCont)
     clearInputs()
 }
 
@@ -24,20 +32,28 @@ function clearInputs() {
     BookDesc.value = ""
 }
 
-function displayBooks() {
-    var box =``
-    for (var i = 0 ; i < bookCont.length ; i++){
+// display book list function
+function displayBooks(arrayContainer) {
+    var box = ``
+    for (var i = 0; i < bookCont.length; i++) {
         box += `<tr>
-                <td>${i+1}</td>
+                <td>${i + 1}</td>
                 <td>${bookCont[i].name}</td>
                 <td>${bookCont[i].price}</td>
                 <td>${bookCont[i].category}</td>
                 <td>${bookCont[i].description}</td>
                 <td>
-                  <button class="btn btn-danger">Delete</button>
+                  <button class="btn btn-danger" onclick="deleteBook(${i})">Delete</button>
                   <button class="btn btn-secondary">Update</button>
                 </td>
               </tr>`
     }
     tableBody.innerHTML = box
+}
+
+// delete book function
+function deleteBook(index) {
+    bookCont.splice(index, 1)
+    localStorage.setItem("allBooks", JSON.stringify(bookCont))
+    displayBooks(bookCont)
 }
