92d6555f564f2929f6219ff7aced0ddff9b0d2e4
Nada-ibrahim12 <123187920+Nada-ibrahim12@users.noreply.github.com>
2024-08-10 21:54:13 +0300

    Done

---
+++ API/js/script.js
@@ -0,0 +1,35 @@
+let xhttp = new XMLHttpRequest();
+xhttp.open('GET', 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=fefdcf3b720b43a3acc7a54ab7da2b18');
+xhttp.send();
+let allData = [];
+let container = document.getElementById('cont');
+
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        let res = JSON.parse(this.responseText);
+        let data = res.articles;
+        allData = data;
+        console.log(allData);
+        displayData(allData);
+    }
+}
+
+function displayData(data) {
+    let box = '';
+    for (let i = 0; i < data.length; i++) {
+        box += `<div class="col-12 col-md-4 my-2">
+            <div class="box text-center">
+              <div class="imgBox">
+                <img src=${data[i].urlToImage} alt="" />
+              </div>
+              <div class=" p-3">
+              <h3 class="title text-center">${data[i].title}</h3>
+              <p class="content ">
+                ${data[i].content || 'No content available'}
+              </p>
+              </div>
+            </div>
+          </div>`;
+    }
+    container.innerHTML = box;
+}
